991643d9d45f1bd301ae8eaac5119813a92b623a
chuaml <chuaml@outlook.com>
2025-01-02 00:18:15 +0800

    amend, add page entry

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@
 
 export default {
     base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                "js-minifier": './js-minifier.html',
+                "example": './example.html',
+            }
+        }
+    }
 }
